aaee136e5292439639993aad70a91164a780cac0
Jayesh1512 <jayeshshetestudy@gmail.com>
2025-07-29 14:39:06 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // ✅ Enables static export
+  distDir: 'build',
+
   eslint: {
     ignoreDuringBuilds: true,
   },
